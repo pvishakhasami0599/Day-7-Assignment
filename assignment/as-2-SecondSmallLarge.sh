69abf19d7b4c8c25cfa57c6a67064f5a12a312ee
pvishakhasami0599 <80875048+pvishakhasami0599@users.noreply.github.com>
2022-02-04 08:52:06 +0530

    Update as-2-SecondSmallLarge.sh

--- assignment/as-2-SecondSmallLarge.sh
+++ assignment/as-2-SecondSmallLarge.sh
@@ -1,4 +1,24 @@
 #!/bin/bash -x
+sort () {
+    for ((i=0; i <= $((${#arr[@]} - 2)); ++i))
+    do
+        for ((j=((i + 1)); j <= ((${#arr[@]} - 1)); ++j))
+        do
+            if [[ ${arr[i]} -gt ${arr[j]} ]]
+            then
+                # echo $i $j ${arr[i]} ${arr[j]}
+                tmp=${arr[i]}
+                arr[i]=${arr[j]}
+                arr[j]=$tmp
+            fi
+        done
+    done
+}
+# arr=(6 5 68 43 82 60 45 19 78 95)
+arr=($(for i in {0..9}; do echo $((RANDOM%900+100)); done))
+echo ${arr[@]}
+sort ${arr[@]}
+echo ${arr[@]}
+echo "Second smallest no is " ${arr[1]}
+echo "Second largest no is " ${arr[8]}
 
-arr=($(for i in {0..9}; do echo $((RANDOM%100)); done))
-echo ${arr[*]}| tr " " "\n" | sort -n | tr "\n" " "
